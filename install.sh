3bf7a118680161059e2243a307e7814a9a13ad65
KVT <kalmonvaladao@gmail.com>
2023-10-18 22:41:32 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3 python3-pip python3-tk xinput xserver-xorg-input-evdev
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+pip3 install numpy
+
+#Calibrador
+git clone https://github.com/reinderien/xcalibrate
+
